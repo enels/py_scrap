3d278c825cb642be1d723ae638b1ba432da0c4a9
enels <enomauwaifo@gmail.com>
2019-08-22 04:24:29 -1200

    Added a try and except statements for catching html link text not found

--- script_test.py
+++ script_test.py
@@ -58,69 +58,90 @@ time.sleep (0.5)
 
 # phone_no_skip_button.click()
 
-# navigate to the google search engine page
-driver.get("https://www.google.com")
 
-# locate search form by name
-search_query = driver.find_element(By.XPATH, "//input[@name='q']")
-
-# send_keys() to search bar
-search_query.send_keys(parameters.search_term)
-time.sleep (10)
-
-# hit the search button
-search_query.send_keys(Keys.RETURN)
-
-# wait for the browser time to load
-time.sleep (10)
-
-# print (type(linked_emails))
-# print ('linkedin emails:')
-# print (linkedin_emails, '\n')
-
-# stores the total number of mails extracted
-total_num_of_mails = 0
-
-import re
 import csv
-with open("C:\\Users\\User\\" + parameters.filename, "w") as result:
-    writer = csv.writer(result, delimiter=",")
 
-    while True:
-        # select the classes (this helps to filter the ads away)
-        linkedin_emails = driver.find_elements_by_class_name('st')
+with open("C:\\Users\\User\\py-scrap\\country_codes", "r") as country_codes:
+    reader = csv.reader(country_codes)
+    country_code = []
 
-        # using python's list comprehension to filter out the mails
-        linkedin_emails = [email.text for email in linkedin_emails]
+    # loops through the csv entries and appends them to a list
+    for row in reader:
+        country_code.append(row[1].lower())
 
-        # check the number of emails found on the current page
-        length = len(linkedin_emails)
+# gets the total number of country codes in list
+total_codes = len(country_code)
 
-        # calculate and store the over sum of emails found during the search
-        total_num_of_mails = total_num_of_mails + length
-        
-        print("Total Number of emails in page: ", length)
-        for text in  linkedin_emails:
+    
+import re
 
-            # extracts the mail
-            email = re.findall("([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)", text)
+# goes through each of the country for that particular query entered by the user
+for index in range(0,total_codes):
+    search_term = 'site:' + country_code[index] + parameters.user_query
 
-            if len(email) > 1 and email[0] == email[1]:
-                # output the user's parameters to a csv file
-                writer.writerow(email[0])
-            else:
-                # output the user's parameters to a csv file
-                writer.writerow(email)
+    # navigate to the google search engine page
+    driver.get("https://www.google.com")
 
-            # writes to stdout
-            print (text, '\n\n')
+    # locate search form by name
+    search_query = driver.find_element(By.XPATH, "//input[@name='q']")
 
-        time.sleep(10)
+    # send_keys() to search bar
+    search_query.send_keys(search_term)
+    time.sleep (10)
+
+    # hit the search button
+    search_query.send_keys(Keys.RETURN)
+
+    # wait for the browser time to load
+    time.sleep (10)
+
+    # stores the total number of mails extracted
+    total_num_of_mails = 0
+    
+    
+    with open("C:\\Users\\User\\" + parameters.filename, "w") as result:
+        writer = csv.writer(result, delimiter=",")
+
+        more_pages = True
+        while more_pages == True:
+            # select the classes (this helps to filter the ads away)
+            linkedin_emails = driver.find_elements_by_class_name('st')
+
+            # using python's list comprehension to filter out the mails
+            linkedin_emails = [email.text for email in linkedin_emails]
+
+            # check the number of emails found on the current page
+            length = len(linkedin_emails)
+
+            # calculate and store the over sum of emails found during the search
+            total_num_of_mails = total_num_of_mails + length
             
-        driver.find_element_by_link_text("Next").click()
-        
-        #break
-        
+            print("Total Number of emails in page: ", length)
+            for text in  linkedin_emails:
+
+                # extracts the mail
+                email = re.findall("([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)", text)
+
+                if len(email) > 1 and email[0] == email[1]:
+                    # output the user's parameters to a csv file
+                    writer.writerow(email[0])
+                else:
+                    # output the user's parameters to a csv file
+                    writer.writerow(email)
+
+                # writes to stdout
+                print (text, '\n\n')
+
+            time.sleep(10)
+
+            try:
+                driver.find_element_by_link_text("Next").click()
+            except:
+                print ("Error: Page not Found")
+                more_pages = False
+            
+            #breakpoint
+            
 print ("Total number of linkedin emails extracted: ", total_num_of_mails)
 print ("End of Search")
 driver.quit()
