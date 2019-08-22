6d06959d6ab8928759ba4961275d3f3e22be27f2
enels <enomauwaifo@gmail.com>
2019-08-22 01:33:10 -1200

    Deleted google linked_in query samples

--- parameters.py
+++ parameters.py
@@ -12,5 +12,4 @@ linkedin_password = "afterall123"
 filename = "user_datas.csv"
 search_term = 'site:de.linkedin.com/pub/ " "Builders" (@gmail.com" OR "@yahoo.com" OR "@hotmail.com")'
 
-# search_term = 'site:ch.linkedin.com/pub/ "CEO" ("@gmail.com" OR "@yahoo.com" OR "@hotmail.com")'
-# search_term = 'site:af.linkedin.com/pub/ "gmail.com" OR "yahoo.com" OR "ymail.com" OR "msn.com" OR "hotmail.com" OR "mac.com" OR "ovimail.com" OR "verizon.com" OR "aol.com" OR "mail.com"'
+
